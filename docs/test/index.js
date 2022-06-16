363d88e326da8487f0b2802c876d72e02e51fb0e
brent <brentfarris@outlook.com>
2022-06-16 13:24:14 -0500

    Adding CSS for index page

--- docs/test/index.js
+++ docs/test/index.js
@@ -1,7 +1,18 @@
 (function() {
 	let body = document.getElementById("content");
+	function create_article(info) {
+		let article = document.createElement("article");
+		article.innerHTML = `<div></div><span>${info.title}</span><p>${info.description}</p>`;
+		if (info.image) {
+			let div = article.firstChild;
+			div.innerHTML = `<img src="${info.image}" />`;
+		}
+		body.appendChild(article);
+	}
+
 	function read(json) {
-		console.log(json);
+		for (let i = 0; i < json.length; ++i)
+			create_article(json[i]);
 	}
 
 	function get_json() {
