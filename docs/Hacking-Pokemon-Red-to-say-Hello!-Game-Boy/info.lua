73459db40447babf708a36a97638bdcd11d288dc
Brent Farris <brentfarris@outlook.com>
2022-06-16 20:38:12 -0500

    Making pokemon hello in Word

---
+++ docs/Hacking-Pokemon-Red-to-say-Hello!-Game-Boy/info.lua
@@ -0,0 +1,9 @@
+return {
+	title = "Hacking Pokemon Red to say Hello! - Game Boy",
+	description = "I decided, for fun, to hack Pokemon Red Version for the Game Boy and change it's code to say hello. I'll go through what I did, how I did it, and even the mistakes I made.
+",
+	keywords = "game boy, gameboy, hacking, reverse engineering, pokemon, pokemon red",
+	image = "https://retroscience.net/Hacking-Pokemon-Red-to-say-Hello!-Game-Boy/view_files/image001.jpg",
+	url = "https://retroscience.net/Hacking-Pokemon-Red-to-say-Hello!-Game-Boy/index.html",
+	date = "2021-11-19"
+}
